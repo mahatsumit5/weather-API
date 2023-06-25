26c3a3a5fa915060de7a9f1f971ef69248c55e10
mahatsumit5 <mahatsumit5@gmail.com>
2023-06-25 21:28:53 +1000

    weather api created

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+const app = express();
+const PORT = 8000;
+app.listen(PORT, () => console.log("server is runiing on port " + PORT));
+
+// middleware to convert data into json
+app.use(express.json());
+
+// cross  origin resources sharing connects two network hosted at different place
+app.use(cors());
+
+app.post("/results", async (req, res) => {
+  const { city } = req.body; //getting name of city from the body
+  console.log(city);
+  const options = {
+    method: "GET",
+    url: "https://weatherapi-com.p.rapidapi.com/forecast.json",
+    params: { q: `${city}`, days: "3" },
+    headers: {
+      "X-RapidAPI-Key": "248de94f44mshef0e6efb0ba66fbp1abdafjsn2422e43de92f",
+      "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+    },
+  };
+  try {
+    const response = await axios.request(options);
+    res.send({
+      status: "success",
+      data: response.data,
+    });
+    console.log(response);
+  } catch (error) {
+    // this if statement solves the error fo cannot set headers after they are sent to the client
+    if (!error) {
+      res.json({
+        status: "error",
+        message: "error",
+      });
+    }
+  }
+
+  // console.error(error);
+});
